8a67ec7a0017f663247505b7f7a6d257b7f6b20d
Nisim Kadosh <nisimkd1@gmail.com>
2025-02-01 18:20:21 +0200

    Add project files.

---
+++ MinStack/MyMinStack.cs
@@ -0,0 +1,46 @@
+﻿namespace MinStack
+{
+    public class MyMinStack
+    {
+        private Stack<int> _stack = new Stack<int>();
+        private Stack<int> _minimumStack = new Stack<int>();
+
+        public void Push(int x)
+        {
+            _stack.Push(x);
+
+            // Add x to the minimum stack only if it will be the new minimum value
+            if (_minimumStack.Count == 0 || x <= _minimumStack.Peek())
+            {
+                _minimumStack.Push(x);
+            }
+        }
+
+        public int Pop()
+        {
+            if (_stack.TryPop(out int res))
+            {
+                int min = _minimumStack.Peek();
+
+                if (min == res)
+                {
+                    _minimumStack.Pop();
+                }
+
+                return res;
+            }
+
+            throw new InvalidOperationException("Stack is empty.");
+        }
+
+        public int GetMin()
+        {
+            if (_minimumStack.TryPeek(out int min))
+            {
+                return min;
+            }
+
+            throw new InvalidOperationException("Stack is empty.");
+        }
+    }
+}
---
+++ MinStack/Program.cs
@@ -0,0 +1,28 @@
+﻿using MinStack;
+
+MyMinStack minStack = new MyMinStack();
+
+/*
+
+minStack.Push(100);
+minStack.Push(200);
+minStack.Push(50);
+
+Console.WriteLine($"GetMin: {minStack.GetMin()}");
+Console.WriteLine($"Pop: {minStack.Pop()}");
+Console.WriteLine($"GetMin: {minStack.GetMin()}");
+
+*/
+
+minStack.Push(100);
+minStack.Push(200);
+minStack.Push(50);
+minStack.Push(300);
+minStack.Push(400);
+
+Console.WriteLine($"GetMin: {minStack.GetMin()}");
+Console.WriteLine($"Pop: {minStack.Pop()}");
+Console.WriteLine($"GetMin: {minStack.GetMin()}");
+Console.WriteLine($"Pop: {minStack.Pop()}");
+Console.WriteLine($"Pop: {minStack.Pop()}");
+Console.WriteLine($"GetMin: {minStack.GetMin()}");
